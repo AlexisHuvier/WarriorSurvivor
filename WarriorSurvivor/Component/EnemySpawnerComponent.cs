dd7e25e7f820cbb0cf8c7e8bf48ceb84d2f59329
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-15 12:57:50 +0200

    Create EnemySpawnerComponent

---
+++ WarriorSurvivor/Component/EnemySpawnerComponent.cs
@@ -0,0 +1,45 @@
+using SharpEngine.Utils;
+using SharpEngine.Utils.Math;
+using WarriorSurvivor.Data;
+using WarriorSurvivor.Entity;
+
+namespace WarriorSurvivor.Component;
+
+public class EnemySpawnerComponent: SharpEngine.Components.Component
+{
+    private double _timer = 0;
+    
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        _timer += gameTime.ElapsedGameTime.TotalSeconds;
+        if (_timer > 0.5)
+        {
+            _timer = 0;
+            var corners = ((Map)GetEntity()).GetCorners();
+
+            var position = Rand.GetRand(4) switch
+            {
+                0 => new Vec2(corners[0].X, Rand.GetRandF(corners[0].Y, corners[3].Y)),
+                1 => new Vec2(Rand.GetRandF(corners[0].X, corners[1].X), corners[0].Y),
+                2 => new Vec2(corners[1].X, Rand.GetRandF(corners[0].Y, corners[3].Y)),
+                3 => new Vec2(Rand.GetRandF(corners[0].X, corners[1].X), corners[2].Y),
+                _ => Vec2.Zero
+            };
+
+            GetEntity().GetScene().AddEntity(new Enemy(position, new EnemyData
+            {
+                Stats = new Stats
+                {
+                    Attack = 3,
+                    Defense = 0,
+                    Life = 10,
+                    Speed = 200
+                },
+                Life = 10,
+                Sprite = "player"
+            }));
+        }
+    }
+}
