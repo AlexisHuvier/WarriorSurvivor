a75e28b6c6379d4b365cc2ab341500065f06fa23
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-15 16:26:05 +0200

    Create Reset function for PlayerData

--- WarriorSurvivor/Data/PlayerData.cs
+++ WarriorSurvivor/Data/PlayerData.cs
@@ -5,11 +5,18 @@ public struct PlayerData
     public Stats Stats;
     public int Life;
     public WeaponData? ActiveWeapon;
-    public readonly WeaponData?[] PassiveWeapons;
+    public WeaponData?[] PassiveWeapons;
 
     public PlayerData()
     {
+        PassiveWeapons = new WeaponData?[5];
+        
         Stats = new Stats();
+        Reset();
+    }
+
+    public void Reset()
+    {
         Life = Stats.Life;
         ActiveWeapon = null;
         PassiveWeapons = new WeaponData?[] { null, null, null, null, null };
