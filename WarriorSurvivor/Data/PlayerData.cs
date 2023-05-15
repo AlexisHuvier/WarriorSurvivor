9e420858da0c17ffef71a446e5da2128169700f1
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-15 12:56:54 +0200

    Add Life to PlayerData

--- WarriorSurvivor/Data/PlayerData.cs
+++ WarriorSurvivor/Data/PlayerData.cs
@@ -3,12 +3,14 @@ namespace WarriorSurvivor.Data;
 public struct PlayerData
 {
     public Stats Stats;
+    public int Life;
     public WeaponData? ActiveWeapon;
     public readonly WeaponData?[] PassiveWeapons;
 
     public PlayerData()
     {
         Stats = new Stats();
+        Life = Stats.Life;
         ActiveWeapon = null;
         PassiveWeapons = new WeaponData?[] { null, null, null, null, null };
     }
