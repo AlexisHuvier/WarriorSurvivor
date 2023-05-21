08b85132aff1909118aa270c6d7c27b5e5cb6729
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-21 16:34:09 +0200

    Update MainMenu

--- WarriorSurvivor/Scene/MainMenu.cs
+++ WarriorSurvivor/Scene/MainMenu.cs
@@ -8,12 +8,12 @@ public class MainMenu: SharpEngine.Scene
     public MainMenu()
     {
         AddWidget(new Label(new Vec2(600, 200), "Warrior Survivor", "big"));
-        AddWidget(new Button(new Vec2(600, 350), "Jouer", "small", new Vec2(200, 50))).Command =
+        AddWidget(new Button(new Vec2(600, 350), "Jouer", "small", new Vec2(250, 50))).Command =
             _ => GetWindow().IndexCurrentScene = 2;
-        AddWidget(new Button(new Vec2(600, 450), "Supprimer Data", "small", new Vec2(200, 50))).Command =
+        AddWidget(new Button(new Vec2(600, 450), "Supprimer Données", "small", new Vec2(250, 50))).Command =
             _ => WS.SaveManager.Reset();
-        AddWidget(new Button(new Vec2(600, 550), "Options", "small", new Vec2(200, 50)));
-        AddWidget(new Button(new Vec2(600, 650), "Quitter", "small", new Vec2(200, 50))).Command =
+        AddWidget(new Button(new Vec2(600, 550), "Options", "small", new Vec2(250, 50)));
+        AddWidget(new Button(new Vec2(600, 650), "Quitter", "small", new Vec2(250, 50))).Command =
             _ => GetWindow().Stop();
         
         WS.SaveManager.Init();
