16d1003cb2060214b735eaffc41238579d4ac95c
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-05-28 16:57:12 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 rmdir /s /q build
 
-dotnet publish WarriorSurvivor/WarriorSurvivor.csproj -c Release -r win-x64 /p:PublishSingleFile=true -o build/WarriorSurvivor
+dotnet publish WarriorSurvivor/WarriorSurvivor.csproj -c Release --self-contained -r win-x64 /p:PublishSingleFile=true -o build/win/WarriorSurvivor
+dotnet publish WarriorSurvivor/WarriorSurvivor.csproj -c Release --self-contained -r linux-x64 /p:PublishSingleFile=true -o build/linux/WarriorSurvivor
